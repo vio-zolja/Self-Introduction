3caf7e8e352df8e927db3f0c5b26ada24e8e315e
Your Name <you@example.com>
2021-05-27 21:49:32 +0200

    css

--- src/homepage.js
+++ src/homepage.js
@@ -15,25 +15,39 @@ export default function Homepage(){
          I made this page exclusivly for You to show You what I can do.
         </h4>  <br /> <hr />
 
-        <h5>Without further ado...</h5>
+        <div className="row">
 
-        <p>While I was writing the application I strugelld to convince You of the things I can do. And after hours of thinking it hit me: "Why don't I <span className="big">leiterally</span> show then what I can do?!"</p>
-        <br />
-        <p>
-            I started the <a href="https://shecodes.io" target="_blank" >SheCodes</a> workshops in 2020 and started my coding journey. 
-            Since I'm self thought the knowlage comes from various sources. Till now I had some experiance with Java Script but I plan on so much more.  
+            <div className="col-7">
+
+                <h5>Without further ado...</h5>
+
+                <p>While I was writing the application I strugelld to convince You of the things I can do. And after hours of thinking it hit me: "Why don't I <span className="big">leiterally</span> show then what I can do?!"</p>
+                <br />
+                <p>
+                    I started the <a href="https://shecodes.io" target="_blank" >SheCodes</a> workshops in 2020 and started my coding journey. 
+                    Since I'm self thought the knowlage comes from various sources. Till now I had some experiance with Java Script but I plan on so much more.  
+                    
+                    <br /><br /><br />
+                </p>
+
+                
             
-             <br /><br />
-        </p>
+            </div>
 
-        <h6>This project was made in <span className="big">Visual Studio Code</span> with <span className="big">React</span></h6>
+            <div className="col-5">
 
-        <br /> <br />
-
-        <img className="react" src={react} alt="a laptop with code" />
-
-
-        <p>The code in open to everyone on <a href="">my Github profile</a></p>
+               
+            <img className="react" src={react} alt="a laptop with code" /> <br /> <br />
+            <h6>This project was made in <span className="big">Visual Studio Code</span> with <span className="big">React</span></h6>
+    
+            </div>
+            
+            <p className="src">The code in open to everyone on my <a href="https://github.com/vio-zolja/Self-Introduction" target="_blank">Github profile</a></p>
+            </div>
         </div>
+
+        
+
+    
     )
 }
